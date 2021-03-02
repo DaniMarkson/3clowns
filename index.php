4eaf3e573babcd0c7c6625edda67a0d40012c719
DaniMarkson <76174346+DaniMarkson@users.noreply.github.com>
2021-03-02 16:07:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="/styles.css">
+    <title>Три Клоуна</title>
+  </head>
+  <body>
+    <header>
+        <div>
+            <div class="main-text">
+                <div class="main-title">ТРИ КЛОУНА</div>
+                <div class="main-description">"Три клоуна" - первый цирк, не имеющий конферансье. Интерактивная программа проходит под рассказ трёх сменяющих друг друга клоунов</div>
+            </div>
+            <div class="main-logo"><img src="/icon.png" alt=""></div>
+            <div class="menu">
+            <a href="/category/clowns/"><div class="item">Категории артистов</div></a>
+            <a href="/place/"><div class="item">Цирковые площадки</div></a>
+            </div>
+        </div>
+    </header>
+    <div class="page">
+        <div class="content">
+            <h1>История цирка</h1>
+            <p>
+                Цирк был основан в далеком 2020 году, в разгар пандемии коронавируса, когда трое начинающих артистов во время охоты поймали диковинного зверя - Мудрую Макаку. В обмен на жизнь зверь поделился с ними своей мудростью. 
+            </p>
+            <img src="3klouna.jpg">
+        </div>
+    </div>
+    
+    
+    <footer>
+        <div class="copyrights">2021 г. Все права сохранены</div>
+        <div class="contacts">Мы Вконтакте - <img src="/vk.png" alt=""></div>
+    </footer>
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  </body>
+</html>
